08e008041c857dbc242ff852f9c10a0eba7e44fb
AnnonymousReview <32741227+AnnonymousForPapers@users.noreply.github.com>
2025-01-30 12:56:29 -0500

    Add files via upload

---
+++ prontoqa/SmolLM2_135M_Instruct/3shot/run_analyze_SmolLM2-135M-Instruct-3shot.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#SBATCH --partition=general               # Name of Partition
+#SBATCH --output=R-%x_%j.out
+
+module purge
+
+source /gpfs/homefs1/ych22001/miniconda3/etc/profile.d/conda.sh
+
+conda activate HF_LLM
+
+printf "1. ModusPonens\n"
+
+python analyze_results.py SmolLM2-135M-Instruct_modusponens_1hop_ProofsOnly_3shot_nodistractor.log
+
+printf "\n\n2. AndIntro\n"
+
+python analyze_results.py SmolLM2-135M-Instruct_andintro_1hop_AndIntro_3shot_nodistractor.log
+
+printf "\n\n3. AndElim\n"
+
+python analyze_results.py SmolLM2-135M-Instruct_andelim_1hop_AndElim_3shot_nodistractor.log
+
+printf "\n\n4. OrIntro\n"
+
+python analyze_results.py SmolLM2-135M-Instruct_orintro_1hop_OrIntro_3shot_nodistractor.log
+
+printf "\n\n5. OrElim\n"
+
+python analyze_results.py SmolLM2-135M-Instruct_orelim_1hop_OrElim_3shot_nodistractor.log
+
+printf "\n\n6. ProofByContra\n"
+
+python analyze_results.py SmolLM2-135M-Instruct_proofbycontra_1hop_ProofByContra_3shot_nodistractor.log
