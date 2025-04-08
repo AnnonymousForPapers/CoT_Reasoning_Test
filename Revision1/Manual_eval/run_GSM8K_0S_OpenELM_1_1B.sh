1d85754c0d7e15431436ab2797af1d8992eec85e
AnnonymousReview <32741227+AnnonymousForPapers@users.noreply.github.com>
2025-04-07 20:56:42 -0400

    Add files via upload

---
+++ Revision1/Manual_eval/run_GSM8K_0S_OpenELM_1_1B.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --partition=general-gpu               # Name of Partition
+#SBATCH --gres=gpu:1                          # Request 1 GPU cards for the job
+#SBATCH --constraint=a100
+#SBATCH --mem=256G                            # Request 256GB of available RAM
+#SBATCH --output=R-%x_%j.out
+
+module purge
+
+source /gpfs/homefs1/ych22001/miniconda3/etc/profile.d/conda.sh
+
+conda activate HF_LLM
+
+python GSM8K_0_shots.py \
+  --model_name "apple/OpenELM-1_1B"  \
+  --shots 0
